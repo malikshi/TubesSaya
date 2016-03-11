63f8d6b26eea3e4c72fcdbe7243f6758e9c67b96
Malik <malikshi@student.telkomuniversity.ac.id>
2016-03-12 00:27:31 +0700

    Update Anggota.java

--- JavaApplication1/src/javaapplication1/Anggota.java
+++ JavaApplication1/src/javaapplication1/Anggota.java
@@ -12,17 +12,48 @@ package javaapplication1;
 public class Anggota extends Orang {
     private long idAnggota;
     private String alamat;
+
+    public long getIdAnggota() {
+        return idAnggota;
+    }
+
+    public void setIdAnggota(long idAnggota) {
+        this.idAnggota = idAnggota;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Anggota(String nama, String noTlp, String email) {
+        super(nama, noTlp, email);
+    }
     
     public void Daftar(String idAnggota, String nama, long noTlp, String email) {
         
     }
     
-    public void viewDaftarPinjam(String idBuku, String namaBuku, String kondisi) {
-        
+        public void viewDaftarPinjam(String idBuku, String namaBuku, String kondisi) {
+        int i=0;
+        do {    
+            System.out.println("ID Buku     : " +Buku[i].idBuku);
+            System.out.println("Nama Buku   : " +Buku[i].namaBuku);
+            System.out.println("Kondisi     : " +Buku[i].kondisi);
+            i = i+1;
+        while(Buku[i]!=null) }     
     }
     
     public void viewDaftarBarangPinjam(String idBuku, String namaBuku, String tanggal) {
-        
+        do {    
+            System.out.println("ID      : " +Buku[i].idBuku);
+            System.out.println("Nama    : " +Buku[i].namaBuku);
+            System.out.println("Tanggal : " +Buku[i].tanggal);
+            i = i+1;
+        while(Buku[i]!=null)
     }
     
     public void setAnggota(String nama, long idAnggota) {
@@ -33,4 +64,7 @@ public class Anggota extends Orang {
     public String getAnggota() {
         return nama;
     }
+    
+    
+    
 }
