1fea858f8957d2486fc81fe772ee1a48badca751
Malik <malikshi@student.telkomuniversity.ac.id>
2016-03-27 19:44:38 +0700

    Update Anggota.java

--- JavaApplication1/src/javaapplication1/Anggota.java
+++ JavaApplication1/src/javaapplication1/Anggota.java
@@ -29,40 +29,41 @@ public class Anggota extends Orang {
         this.alamat = alamat;
     }
 
-    public Anggota(String nama, String noTlp, String email) {
+    public Anggota(long idAnggota,  String nama, String noTlp, String email) {
         super(nama, noTlp, email);
+        this.idAnggota=idAnggota;
     }
     
-    public void Daftar(String idAnggota, String nama, long noTlp, String email) {
-        
-    }
-    
-        public void viewDaftarPinjam(String idBuku, String namaBuku, String kondisi) {
-        int i=0;
-        do {    
-            System.out.println("ID Buku     : " +Buku[i].idBuku);
-            System.out.println("Nama Buku   : " +Buku[i].namaBuku);
-            System.out.println("Kondisi     : " +Buku[i].kondisi);
-            i = i+1;
-        while(Buku[i]!=null) }     
-    }
-    
-    public void viewDaftarBarangPinjam(String idBuku, String namaBuku, String tanggal) {
-        do {    
-            System.out.println("ID      : " +Buku[i].idBuku);
-            System.out.println("Nama    : " +Buku[i].namaBuku);
-            System.out.println("Tanggal : " +Buku[i].tanggal);
-            i = i+1;
-        while(Buku[i]!=null)
-    }
+//    public void Daftar(String idAnggota, String nama, long noTlp, String email) {
+//        
+//    }
+//    
+//        public void viewDaftarPinjam(String idBuku, String namaBuku, String kondisi) {
+//        int i=0;
+//        do {    
+//            System.out.println("ID Buku     : " +Buku[i].idBuku);
+//            System.out.println("Nama Buku   : " +Buku[i].namaBuku);
+//            System.out.println("Kondisi     : " +Buku[i].kondisi);
+//            i = i+1;
+//        while(Buku[i]!=null) }     
+//    }
+//    
+//    public void viewDaftarBarangPinjam(String idBuku, String namaBuku, String tanggal) {
+//        do {    
+//            System.out.println("ID      : " +Buku[i].idBuku);
+//            System.out.println("Nama    : " +Buku[i].namaBuku);
+//            System.out.println("Tanggal : " +Buku[i].tanggal);
+//            i = i+1;
+//        while(Buku[i]!=null)
+//    }
     
     public void setAnggota(String nama, long idAnggota) {
-        this.nama = nama;
+        super.setNama(nama);
         this.idAnggota = idAnggota;
     }
     
     public String getAnggota() {
-        return nama;
+        return super.getNama();
     }
     
     
