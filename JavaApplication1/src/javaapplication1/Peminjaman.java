cdb90930863b1ad5c452166c634e8bec8dd98231
Malik <malikshi@student.telkomuniversity.ac.id>
2016-03-27 19:48:58 +0700

    Update Peminjaman.java

--- JavaApplication1/src/javaapplication1/Peminjaman.java
+++ JavaApplication1/src/javaapplication1/Peminjaman.java
@@ -5,31 +5,97 @@
  */
 package javaapplication1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Synricha
  */
 public class Peminjaman {
-    private static  Buku[] pinjaman = new Buku[5];
+
+    private ArrayList<Buku> pinjaman = new ArrayList();
     private Anggota peminjam;
-    private int npinjam=0;
-    
-    public void addPinjaman(Buku b){
-        if(npinjam<5){
-            this.pinjaman[npinjam]= b;
+    private long idTransaksi;
+    private static long tmpId = 1;
+    private int npinjam = 0;
+
+    public Peminjaman() {
+        this.idTransaksi = tmpId;
+        autoIncrement();
+    }
+
+    public long getIdTransaksi() {
+        return idTransaksi;
+    }
+
+    public void setIdTransaksi(long idTransaksi) {
+        this.idTransaksi = idTransaksi;
+    }
+
+    public void autoIncrement() {
+        tmpId++;
+    }
+
+    public ArrayList<Buku> getPinjaman() {
+        return pinjaman;
+    }
+
+    public void setPinjaman(ArrayList<Buku> pinjaman) {
+        this.pinjaman = pinjaman;
+    }
+
+    public Anggota getPeminjam() {
+        return peminjam;
+    }
+
+    public void setPeminjam(Anggota peminjam) {
+        this.peminjam = peminjam;
+    }
+
+    public int getNpinjam() {
+        return npinjam;
+    }
+
+    public void setNpinjam(int npinjam) {
+        this.npinjam = npinjam;
+    }
+
+    public boolean delBuku(String idBuku) {
+        boolean iscek = false;
+        for (int i = 0; i < pinjaman.size(); i++) {
+            if (pinjaman.get(i).getIdBuku() == idBuku) {
+                pinjaman.remove(i);
+                iscek = true;
+            }
+        }
+        if (iscek == true) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void addPinjaman(Buku b) {
+        if (npinjam < 5) {
+            pinjaman.add(b);
             npinjam++;
         }
     }
-    public int getJumlahPinjam(){
+
+    public int getJumlahPinjam() {
         return npinjam;
     }
-    public Buku getBuku(int i){
-        return pinjaman[i];
+
+    public Buku getBuku(int i) {
+        return pinjaman.get(i);
     }
-    public void setAnggota(Anggota a){
-        this.peminjam=a;
+
+    public void setAnggota(Anggota a) {
+        this.peminjam = a;
     }
-    public Anggota getAnggota(){
+
+    public Anggota getAnggota() {
         return peminjam;
     }
 }
